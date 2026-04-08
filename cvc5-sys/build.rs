5f225cd18d89a281fad83d9e6f7ab09e87ea03f8
Jason Hu <zhonhu@amazon.com>
2026-04-08 13:52:46 -0700

    specify target when building to avoid linking issues

--- cvc5-sys/build.rs
+++ cvc5-sys/build.rs
@@ -309,8 +309,14 @@ fn ensure_cvc5_built(cvc5_dir: &PathBuf) {
         .map(|n| n.get().to_string())
         .unwrap_or_else(|_| "4".to_string());
 
+    let mut targets: Vec<&str> = vec!["cvc5"];
+    if cfg!(feature = "parser") {
+        targets.push("cvc5parser");
+    }
+
     let status = Command::new("make")
         .arg(format!("-j{jobs}"))
+        .args(targets)
         .current_dir(cvc5_dir.join("build"))
         .status()
         .expect("Failed to run make");
